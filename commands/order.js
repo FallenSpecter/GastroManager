3de71e08a371b0fc9904ef169496a7f26c19ccda
fallenspecter <luke.lesniak@gmail.com>
2017-05-14 15:25:59 +0200

    Added error message for unsupported restaurants

--- commands/order.js
+++ commands/order.js
@@ -1,9 +1,9 @@
 var restUtil = require('../restUtil');
 var util = require('../util');
 var _ = require('underscore');
-var config = require ('../bin/config');
-var ordersDao = require ('../bin/ordersDao');
-var dateUtil = require ('../bin/dateUtil');
+var config = require('../bin/config');
+var ordersDao = require('../bin/ordersDao');
+var dateUtil = require('../bin/dateUtil');
 var moment = require('moment');
 var commonResp = require('../bin/commonResp');
 var configHelper = require('../bin/configHelper');
@@ -13,48 +13,53 @@ var timeWindowEnd = config.settings.timeWindowEnd;
 var drawTimeWindow = config.settings.drawTime;
 var people = configHelper.getPeople();
 
-module.exports = function order(param){
+module.exports = function order(param) {
 
-    restUtil.getUser(param.user).then(function(user){
-        if(_.contains(people, user)){
+    restUtil.getUser(param.user).then(function (user) {
+        if (_.contains(people, user)) {
             user = {id: param.user, name: user};
             orderProcessing(user, param);
-        }else{
+        } else {
             commonResp.sendUserUnregistered(param.channel);
         }
     });
 
 };
 
-function orderProcessing(user, param){
+function orderProcessing(user, param) {
     var now = moment();
-    var startTime= dateUtil.formatToDate(now, timeWindowStart);
+    var startTime = dateUtil.formatToDate(now, timeWindowStart);
     var endTime = dateUtil.formatToDate(now, timeWindowEnd);
     var drawTime = dateUtil.formatToDate(now, drawTimeWindow);
 
-    if(param.args.length >= 2 & param.args[0]==='remove'){
-        if(dateUtil.isInTimeWindow(now,startTime,drawTime)) {
+    if (param.args.length >= 2 & param.args[0] === 'remove') {
+        if (dateUtil.isInTimeWindow(now, startTime, drawTime)) {
             var restaurant = param.args[1];
-            if(ordersDao.removeOrderFromRestaurant(restaurant, user)) {
+            if (ordersDao.removeOrderFromRestaurant(restaurant, user)) {
                 util.postMessage(param.channel, 'Grazie, Twoje zamówienie zostało usunięte, mamma mia.');
             }
-        }else {
+        } else {
             util.postMessage(param.channel,
                 'Scusi zamówienia mogą być usuwane tylko między ' + timeWindowStart + ' a ' + drawTimeWindow);
         }
 
-    }else if(param.args.length >= 3 & param.args[0]=== 'place'){
-        if(dateUtil.isInTimeWindow(now,startTime,endTime)){
+    } else if (param.args.length >= 3 & param.args[0] === 'place') {
+        var registeredRestaurants = _.map(config.settings.places, function (e) {
+            return e.name;
+        });
+        if (!_.contains(registeredRestaurants, param.args[1])) {
+            util.postMessage(param.channel, 'Scusi restauracja ' + param.args[1] + ' nie jest obsługiwana');
+        } else if (dateUtil.isInTimeWindow(now, startTime, endTime)) {
             var restaurant = param.args[1];
             var dish = param.args.splice(2).join(' ');
             var resp = '';
-            if(ordersDao.addOrderToRestaurant(restaurant, user, dish)){
+            if (ordersDao.addOrderToRestaurant(restaurant, user, dish)) {
                 resp = 'Che bello!! Twoje zamównienie zostało złożone.';
-            } else{
+            } else {
                 resp = user.name + ' tranquillo, masz już zamówienie: ' + ordersDao.getOrderFromRestaurant(restaurant, user);
             }
             util.postMessage(param.channel, resp);
-        }else {
+        } else {
             util.postMessage(param.channel,
                 'Scusi zamówienia mogą być składane tylko między ' + timeWindowStart + ' a ' + timeWindowEnd);
         }
